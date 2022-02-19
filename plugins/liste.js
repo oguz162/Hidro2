7ba5a2b66e7e3de5efcdbb803f1aab96f255c694
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-02-19 18:36:06 +0300

    Update and rename amru.js to liste.js

--- plugins/liste.js
+++ plugins/liste.js
@@ -9,7 +9,7 @@ var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
-Asena.addCommand({pattern: 'list', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'liste', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 // buton mesajı gönderirir Zlose
     var plk_say = new Date().toLocaleString('TR', { timeZone: 'Europe/Istanbul' }).split(' ')[1]
 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -18,13 +18,13 @@ var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '
 	const buttons = [
 
         {buttonId: 'id1', buttonText: {displayText: '☃️düzenlenecek❄️'}, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: 'düzenlenecek'}, type: 1},
-        {buttonId: 'id3', buttonText: {displayText: 'düzenlenecek\n\n```❄️```\n'  }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: '☃️düzenlenecek❄️'}, type: 1},
+        {buttonId: 'id3', buttonText: {displayText: '☃️düzenlenecek❄️'}, type: 1},
 
       ]
       
       const buttonMessage = {
-          contentText: ' selam dostum---🧚\n\n```Bᴏᴛ_Nᴀᴍᴇ:``` *'+Config.LIST+'*\n\n💙 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🥶 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n☃️ menü için aşağıdaki butonlara tıklayınız :)\n', 
+          contentText: ' selam dostum---🧚\n\n```Bᴏᴛ Nᴀᴍᴇ::``` *'+Config.BOT_NAME+'*\n\n💙 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🥶 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n☃️ menü için aşağıdaki butonlara tıklayınız :)\n', 
           footerText: '✮ Zlose 💕',
           buttons: buttons,
           headerType: 1
