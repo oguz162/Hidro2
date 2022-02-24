11d038aac655bde85386f969c4e49c3806b19b78
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-02-25 00:29:23 +0300

    Update and rename erorrnyt.js to Memes.js

--- plugins/Memes.js
+++ plugins/Memes.js
@@ -1,11 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsMyPnky - Yusuf Usta
-Coded by @KursadHD
-re edited by afnanplk
-*/
-
 const MyPnky = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const memeMaker = require('meme-maker')
