73ed2f6e8819d074f3fe24e7bf66f29420f4f30c
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-03-07 01:10:44 +0300

    Update butonalive.js

--- plugins/butonalive.js
+++ plugins/butonalive.js
@@ -1,7 +1,3 @@
-/* Amalser Bot
-Re-edit Amalser + chunkindepadayali
-*/
-
 const Asena = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
@@ -13,24 +9,26 @@ const os = require('os');
 var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
+var sk1,sk2
+var split = Config.KAZTRO_SER.split('/');
+         sk2 = split[1];
+         sk1 = split[0];
 
-
-let whb = Config.WORKTYPE == 'private' ? false : true
 
 Asena.addCommand({pattern: 'alive', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-// send a buttons message!credit Amal
+// send a buttons message!credit Ajayan
     const buttons = [
-        {buttonId: 'id1', buttonText: {displayText: Config.KAZTRO_SER }, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: Config.ALIVEMSG }, type: 1}
+        {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: sk2 }, type: 1}
       ]
       
       const buttonMessage = {
-          contentText: '```'+Config.KAZTRO_SER+'\n\n```'+Config.ALIVEMSG+'\n',
-          footerText: 'ᴀᴍʀᴜ sᴇʀ™ ',
+          contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
+          footerText: 'Zlose',
           buttons: buttons,
           headerType: 1
       }
       
-await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 123, status: 200, thumbnail: fs.readFileSync('https://www.linkpicture.com/q/20220115_085729.jpg'), surface: 200, message: Config.KAZTRO_SER, orderTitle: Config.KAZTRO_SER, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.KAZTRO_SER + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('https://www.linkpicture.com/q/20220115_085729.jpg')}}}});
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
 
 }));
