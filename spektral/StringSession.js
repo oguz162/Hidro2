5deadbb13e830e2b57674b13f0c0b012d3563bc4
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-03-26 21:46:08 +0300

    Update StringSession.js

--- spektral/StringSession.js
+++ spektral/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('AMRUSER_CODE' in process.env && string === undefined) {
+        if ('SPEKTRAL_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
