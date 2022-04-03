e02a969b5204a22acab30d7da3e17e1d7712ffff
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-04-03 19:04:03 +0300

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -15,23 +15,6 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
-const Asena = require('../events');
-const Config = require('../config');
-
-const Language = require('../language');
-const Lang = Language.getString('admin');
-const mut = Language.getString('mute');
-
-async function checkImAdmin(message, user = message.client.user.jid) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {
-        
-        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
-
 Asena.addCommand({pattern: 'ban ?(.*)', fromMe: true, desc: Lang.BAN_DESC}, (async (message, match) => {  
     if (message.jid.endsWith('@g.us')) {
     var im = await checkImAdmin(message);
