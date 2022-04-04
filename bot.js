5a16280f079d43ff2db1167496b7cdcb5c3d97d2
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-04-05 00:11:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ async function whatsAsena () {
             let min = getGMTm < 10 ? '0' + getGMTm : getGMTm
             const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
             var utch = new Date().toLocaleDateString(config.lANG, get_localized_date)
-            const biography = '📅 ' + utch + '\n'⏱ ' + hour + ':' + min + ':' + getGMTs + '\n\n🌑 spektral'
+            const biography = '📅 ' + utch + '\n⏱ ' + hour + ':' + min + ':' + getGMTs + '\n\n🌑 spektral'
             await conn.setStatus(biography)
         }
     }, 12368);
