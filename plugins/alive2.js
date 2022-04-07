f407406397b3b9980c35e3156bac122aa7eaa50d
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-04-07 23:23:04 +0300

    Update alive2.js

--- plugins/alive2.js
+++ plugins/alive2.js
@@ -1,4 +1,4 @@
-const Asena = require('../events');
+/*const Asena = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
@@ -33,3 +33,4 @@ Asena.addCommand({pattern: 'buton', fromMe: true, dontAddCommandList: true}, (as
        await message.client.sendMessage(message.jid, button, MessageType.buttonMessage)
     
     }));
+/*
