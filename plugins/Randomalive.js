46672f5059dc8c8d05875a1b50179c96db4689cf
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-04-07 23:22:32 +0300

    Update Randomalive.js

--- plugins/Randomalive.js
+++ plugins/Randomalive.js
@@ -1,4 +1,4 @@
-const Asena = require('../events');
+/*const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
@@ -57,3 +57,4 @@ const Lang = Language.getString('system_stats');
         )
   
 }));
+*/
