668cb8f9f6698e6329ecd4815ca9f67a97b46eed
unique_hunter <77807213+oguz162@users.noreply.github.com>
2022-05-01 01:33:26 +0300

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -5,6 +5,34 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('afk');
 
+var r_text = new Array ();
+        r_text[0] = "Şu an acele işim var, daha sonra mesaj atsan olmaz mı? Zaten yine geleceğim.";
+        r_text[1] = "Aradığınız kişi şu anda telefona cevap veremiyor. Sinyal sesinden sonra kendi tarifeniz üzerinden mesajınızı bırakabilirsiniz. Mesaj ücreti 49 kuruştur. \n`biiiiiiiiiiiiiiiiiiiiiiiiiiiiip`!";
+        r_text[2] = "Birkaç dakika içinde geleceğim. Fakat gelmezsem...\ndaha fazla bekle.";
+        r_text[3] = "Şu an burada değilim, ama muhtemelen başka bir yerdeyim.";
+        r_text[4] = "Güller kırmızı\nMenekşeler mavi\nBana bir mesaj bırak\nVe sana döneceğim.";
+        r_text[5] = "Bazen hayattaki en iyi şeyler beklemeye değer…\nHemen dönerim.";
+        r_text[6] = "Hemen dönerim,\nama eğer geri dönmezsem,\ndaha sonra dönerim.";
+        r_text[7] = "Henüz anlamadıysan,\nburada değilim.";
+        r_text[8] = "Merhaba, uzak mesajıma hoş geldiniz, bugün sizi nasıl görmezden gelebilirim?";
+        r_text[9] = "7 deniz ve 7 ülkeden uzaktayım,\n7 su ve 7 kıta,\n7 dağ ve 7 tepe,\n7 ovala ve 7 höyük,\n7 havuz ve 7 göl,\n7 bahar ve 7 çayır,\n7 şehir ve 7 mahalle,\n7 blok ve 7 ev...\n\nMesajların bile bana ulaşamayacağı bir yer!";
+        r_text[10] = "Şu anda klavyeden uzaktayım, ama ekranınızda yeterince yüksek sesle çığlık atarsanız, sizi duyabilirim.";
+        r_text[11] = "Şu yönde ilerliyorum\n---->";
+        r_text[12] = "Şu yönde ilerliyorum\n<----";
+        r_text[13] = "Lütfen mesaj bırakın ve beni zaten olduğumdan daha önemli hissettirin.";
+        r_text[14] = "Sahibim burada değil, bu yüzden bana yazmayı bırak.";
+        r_text[15] = "Burada olsaydım,\nSana nerede olduğumu söylerdim.\n\nAma ben değilim,\ngeri döndüğümde bana sor...";
+        r_text[16] = "Uzaklardayım!\nNe zaman dönerim bilmiyorum !\nUmarım birkaç dakika sonra!";
+        r_text[17] = "Sahibim şuan da müsait değil. Adınızı, numarınızı ve adresinizi verirseniz ona iletibilirm ve böylelikle geri döndüğü zaman.";
+        r_text[18] = "Üzgünüm, sahibim burada değil.\nO gelene kadar benimle konuşabilirsiniz.\nSahibim size sonra döner.";
+        r_text[19] = "Bahse girerim bir mesaj bekliyordun!";
+        r_text[20] = "Hayat çok kısa, yapacak çok şey var...\nOnlardan birini yapıyorum...";
+        r_text[21] = "Şu an burada değilim....\nama öyleysem ...\n\nbu harika olmaz mıydı?";
+        r_text[22] = "Beni hatırladığına sevindim ama şuanda klavye bana çok uzak";
+        r_text[23] = "Belki İyiyim, Belki Kötüyüm Bilmiyorsun Ama AFK Olduğumu Görebiliyorsun";
+
+        var i = Math.floor(24*Math.random())
+
 var AFK = {
     isAfk: false,
     reason: false,
@@ -32,19 +60,19 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
             if (message.jid.includes('-') && (message.mention !== false && message.mention.length !== 0)) {
                 message.mention.map(async (jid) => {
                     if (message.client.user.jid.split('@')[0] === jid.split('@')[0]) {
-                        await message.client.sendMessage(message.jid,Lang.AFK_TEXT + '\n' + 
+                        await message.client.sendMessage(message.jid,'${r_text[i]}\n' + 
                         (AFK.reason !== false ? '\n*' + Lang.REASON + ':* ```' + AFK.reason + '```' : '') + 
                         (AFK.lastseen !== 0 ? '\n*' + Lang.LAST_SEEN + ':* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + Lang.AGO : ''), MessageType.text, {quoted: message.data});            
                     }
                 })
             } else if (message.jid.includes('-') && message.reply_message !== false) {
                 if (message.reply_message.jid.split('@')[0] === message.client.user.jid.split('@')[0]) {
-                    await message.client.sendMessage(message.jid,Lang.AFK_TEXT + '\n' + 
+                    await message.client.sendMessage(message.jid,'${r_text[i]}\n' + 
                         (AFK.reason !== false ? '\n*' + Lang.REASON + ':* ```' + AFK.reason + '```' : '') + 
                         (AFK.lastseen !== 0 ? '\n*' + Lang.LAST_SEEN + ':* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + Lang.AGO : ''), MessageType.text, {quoted: message.data});
                 }
             } else {
-                await message.client.sendMessage(message.jid,Lang.AFK_TEXT + '\n' + 
+                await message.client.sendMessage(message.jid,'${r_text[i]}\n' + 
                 (AFK.reason !== false ? '\n*' + Lang.REASON + ':* ```' + AFK.reason + '```' : '') + 
                 (AFK.lastseen !== 0 ? '\n*' + Lang.LAST_SEEN + ':* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + Lang.AGO : ''), MessageType.text, {quoted: message.data});
             }
